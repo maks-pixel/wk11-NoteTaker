0f9058cbab5d67cc5f7dbecb816facf4b4be7095
Makda Girmay <makdagirmay4@gmail.com>
2021-12-18 20:59:30 -0500

    setting up routes to html pages

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { notes } = require('./db/db.json');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.Port || 3001
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 // app.get('/api/notes', (req, res) => {
 //   let results = notes;
