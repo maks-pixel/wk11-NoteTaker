30a98ed10d5e35e797b1e7dc5c821b78d92ad6bd
Makda Girmay <makdagirmay4@gmail.com>
2021-12-20 01:47:06 -0500

    deploying heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const PORT = process.env.Port || 3001
+const PORT = process.env.PORT || 3001
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
